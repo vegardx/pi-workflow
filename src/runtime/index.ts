e2bb5230d22f9929f4cdeb94f6b3ebd95bf8ffca
Vegard Hansen <vegard@netwerk.io>
2026-09-16 18:14:08 +0200

    feat!: freeze the 1.0 public API and move engine internals to the runtime subpath

---
+++ src/runtime/index.ts
@@ -0,0 +1,267 @@
+// Runtime internals. Not covered by the 1.0 API freeze; may change in any
+// minor release. See docs/contracts.md "Public API and stability".
+export {
+	projectWorkflowArtifactInputs,
+	readWorkflowArtifactInputs,
+	type VerifiedWorkflowArtifactInput,
+	type VerifyWorkflowArtifactInputOptions,
+	validateWorkflowTaskContext,
+	verifyWorkflowArtifactInputs,
+	WorkflowArtifactInputError,
+	type WorkflowArtifactInputOptions,
+} from "../artifact-input.js";
+export {
+	canonicalArtifactJson,
+	WorkflowArtifactStore,
+	WorkflowArtifactStoreError,
+} from "../artifact-store.js";
+export {
+	type CurrentSubagentAttempt,
+	currentSubagentAttempt,
+	currentSubagentAttemptId,
+	type SettledAgentUsage,
+	settledAgentUsage,
+} from "../attempts.js";
+export {
+	budgetExceededReason,
+	reservedWorkflowUsage,
+	settledWorkflowUsage,
+	type WorkflowReservedUsage,
+	type WorkflowSettledUsage,
+	type WorkflowUsage,
+	type WorkflowUsageOptions,
+	workflowUsage,
+} from "../budget.js";
+export {
+	CHECKPOINT_RUN_ENDING_REASON,
+	cancelOpenWorkflowCheckpoints,
+	createWorkflowCheckpointTaskExecutor,
+	type WorkflowCheckpointDecisionInput,
+	WorkflowCheckpointExecutionError,
+	type WorkflowCheckpointExecutionResult,
+	type WorkflowCheckpointFailureStage,
+	type WorkflowCheckpointRequestOutcome,
+	type WorkflowCheckpointTaskExecutor,
+	type WorkflowCheckpointTaskExecutorOptions,
+	type WorkflowCheckpointTaskOutcome,
+} from "../checkpoint-executor.js";
+export {
+	WorkflowDecisionRecordError,
+	WorkflowDecisionRecordStore,
+} from "../decision-store.js";
+export {
+	DYNAMIC_ASYNC_METHODS,
+	DYNAMIC_CONTEXT_METHODS,
+	DYNAMIC_CONTEXT_PROPERTIES,
+	DYNAMIC_RPC_MESSAGE_TYPES,
+	DYNAMIC_SHIM_EXPORTS,
+	DYNAMIC_SYNC_METHODS,
+	DYNAMIC_VM_ABORT_GRACE_MS,
+	DYNAMIC_VM_CODE_GENERATION,
+	DYNAMIC_VM_RESOURCE_LIMITS,
+	DYNAMIC_VM_SYNC_WAIT_MS,
+	MAX_DYNAMIC_HANDLE_REFS,
+	MAX_DYNAMIC_RPC_ARGS,
+	MAX_DYNAMIC_RPC_MESSAGE_BYTES,
+	MAX_DYNAMIC_RPC_MESSAGES,
+	MAX_DYNAMIC_VM_ERROR_CHARS,
+} from "../dynamic/constants.js";
+export {
+	createDynamicDiscoveredWorkflow,
+	createDynamicWorkflowDefinition,
+	type DynamicDiscoveredWorkflowOptions,
+	type DynamicWorkflowDefinitionOptions,
+} from "../dynamic/definition.js";
+export {
+	DynamicWorkflowExecutionError,
+	type DynamicWorkflowExecutionStage,
+	isDynamicWorkflowExecutionError,
+} from "../dynamic/execution-error.js";
+export {
+	deriveDynamicDefinitionIdentitySha256,
+	deriveDynamicHostApiSha256,
+	deriveDynamicImportPolicySha256,
+} from "../dynamic/identity.js";
+export { WorkflowDynamicStoreError } from "../dynamic/proposal-store.js";
+export {
+	type ExtractDynamicWorkflowManifestOptions,
+	extractDynamicWorkflowManifest,
+} from "../dynamic/vm-host.js";
+export {
+	deriveCheckpointEffectSha256,
+	deriveNestedWorkflowRunId,
+	deriveSubagentOperationId,
+	deriveSubagentResultSha256,
+	deriveSubagentSettlementEvidence,
+	deriveSupportImplementationIdentitySha256,
+	deriveTaskExecutionId,
+	deriveWorkflowArtifactId,
+	deriveWorkflowFailureSha256,
+	deriveWorkflowHandoffDescriptor,
+	type WorkflowHandoffImportProjection,
+} from "../execution.js";
+export {
+	type VerifiedWorkflowHandoff,
+	verifyWorkflowHandoffEvidence,
+	WORKFLOW_HANDOFF_UNVERIFIED_MESSAGE,
+	WORKFLOW_HANDOFF_VERIFICATION_MESSAGES,
+	WorkflowHandoffVerificationError,
+	type WorkflowHandoffVerificationReason,
+} from "../handoff.js";
+export {
+	InvalidWorkflowRunTransitionError,
+	InvalidWorkflowTaskTransitionError,
+	transitionWorkflowRunStatus,
+	transitionWorkflowTaskStatus,
+} from "../lifecycle.js";
+export {
+	deriveAgentTaskIdentity,
+	deriveCheckpointTaskIdentity,
+	deriveNestedWorkflowTaskIdentity,
+	deriveSupportTaskIdentity,
+	deriveWorkflowTaskId,
+	type FinalizerDeclaration,
+	type MaterializationCommit,
+	type NestedWorkflowDeclaration,
+	WorkflowMaterializationError,
+	WorkflowTaskMaterializer,
+	type WorkflowTaskMaterializerOptions,
+} from "../materializer.js";
+export {
+	createWorkflowNestedRunExecutor,
+	type NestedWorkflowTerminalStatus,
+	type WorkflowNestedExecutionResult,
+	type WorkflowNestedLaunchOutcome,
+	WorkflowNestedRunError,
+	type WorkflowNestedRunExecutor,
+	type WorkflowNestedRunExecutorOptions,
+	type WorkflowNestedRunLaunch,
+	type WorkflowNestedRunProvider,
+	type WorkflowNestedRunSettlement,
+	type WorkflowNestedTaskOutcome,
+} from "../nested-run-executor.js";
+export {
+	readWorkflowJournalUnleased,
+	type UnleasedJournalRead,
+	type WorkflowJournalAppendNotice,
+	type WorkflowJournalEvent,
+	WorkflowJournalEventSchema,
+	WorkflowRunJournal,
+	type WorkflowRunJournalOpenOptions,
+	type WorkflowRunSnapshot,
+	WorkflowRunSnapshotSchema,
+} from "../persistence/journal.js";
+export {
+	acquireWorkflowRunLease,
+	probeWorkflowRunLease,
+	WorkflowPersistenceCorruptionError,
+	type WorkflowRunLease,
+	WorkflowRunLeaseFencedError,
+	type WorkflowRunLeaseProbe,
+	type WorkflowRunLeaseRecord,
+	WorkflowRunLeaseRecordSchema,
+	WorkflowRunLeaseUnavailableError,
+} from "../persistence/run-lease.js";
+export {
+	invalidationClosure,
+	rebuildWorkflowSnapshot,
+	reduceWorkflowEvents,
+	WorkflowEventReductionError,
+	type WorkflowInvalidationClosure,
+} from "../reducer.js";
+export {
+	type DiscoveredWorkflow,
+	discoverWorkflows,
+	type WorkflowDefinitionIdentity,
+} from "../registry.js";
+export {
+	admitsInvalidation,
+	availableWorkflowRunActions,
+	awaitsRecovery,
+	deadlinePassed,
+	hasOpenOperatorIntent,
+	isNestedRun,
+	isReopenedTask,
+	OPERATOR_RESUME_REASON,
+	pendingCheckpoints,
+	requiresAttention,
+	resumableTasks,
+	resumeRefusal,
+	retryableTasks,
+	runActionFacts,
+	type WorkflowRunActionFacts,
+} from "../run-actions.js";
+export {
+	compareRunSummaries,
+	decodeWorkflowRunCursor,
+	encodeWorkflowRunCursor,
+	invalidationPreview,
+	pendingCheckpointViews,
+	type RunInspectionOptions,
+	type RunLogOptions,
+	runInspection,
+	runLogs,
+	runSummary,
+	type TaskViewOptions,
+	taskViews,
+	type WorkflowRunCursor,
+} from "../run-projection.js";
+export {
+	type WorkflowRunRecord,
+	WorkflowRunRecordError,
+	WorkflowRunRecordSchema,
+	WorkflowRunRecordStore,
+} from "../run-record.js";
+export {
+	createWorkflowSequentialScheduler,
+	WorkflowSchedulerError,
+	type WorkflowSchedulerOutcome,
+	type WorkflowSchedulerReconcileFacts,
+	type WorkflowSchedulerReconcileOutcome,
+	type WorkflowSequentialScheduler,
+	type WorkflowSequentialSchedulerOptions,
+} from "../scheduler.js";
+export {
+	createStaticWorkflowRuntime,
+	isStaticWorkflowParked,
+	type StaticWorkflowDriveResult,
+	type StaticWorkflowParkedResult,
+	type StaticWorkflowPendingCheckpoint,
+	type StaticWorkflowRunResult,
+	type StaticWorkflowRuntime,
+	StaticWorkflowRuntimeError,
+	type StaticWorkflowRuntimeOptions,
+} from "../static-runtime.js";
+export { supportRegistrationIdentity } from "../support.js";
+export {
+	createWorkflowSupportTaskExecutor,
+	WorkflowSupportExecutionError,
+	type WorkflowSupportExecutionResult,
+	type WorkflowSupportFailureStage,
+	type WorkflowSupportIntentOutcome,
+	type WorkflowSupportTaskExecutor,
+	type WorkflowSupportTaskExecutorOptions,
+	type WorkflowSupportTaskOutcome,
+} from "../support-executor.js";
+export {
+	createWorkflowTaskFinalizer,
+	WorkflowTaskFinalizationError,
+	type WorkflowTaskFinalizationOutcome,
+	type WorkflowTaskFinalizer,
+	type WorkflowTaskFinalizerOptions,
+} from "../task-finalizer.js";
+export {
+	createWorkflowTaskLauncher,
+	WorkflowTaskLaunchError,
+	type WorkflowTaskLauncher,
+	type WorkflowTaskLauncherOptions,
+	type WorkflowTaskLaunchOutcome,
+} from "../task-launcher.js";
+export {
+	createWorkflowTaskRetrier,
+	type WorkflowAttemptDecision,
+	WorkflowAttemptError,
+	type WorkflowAttemptKind,
+	type WorkflowTaskRetrier,
+	type WorkflowTaskRetrierOptions,
+} from "../task-retrier.js";
